ef18bf47990c0f068a3ddcf77f959411b6c099ff
Emanuel <emanuelmarcos9800@gmail.com>
2025-09-25 22:39:15 -0300

    Home page de autor

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,9 @@ import AdminPage from './pages/AdminPage';
 import EventEditionsPage from './pages/EventEditionsPage';
 import ArticleAdminPage from './pages/ArticleAdminPage';
 import BibtexImportPage from './pages/BibtexImportPage';
-import PublicEventPage from './pages/PublicEventPage'; // Nova página
-import PublicEditionPage from './pages/PublicEditionPage'; // Nova página
+import PublicEventPage from './pages/PublicEventPage';
+import PublicEditionPage from './pages/PublicEditionPage';
+import AuthorPage from './pages/AuthorPage'; // Nova página
 
 // Importando componentes de layout
 import Header from './components/Header';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/eventos/:eventSlug" element={<PublicEventPage />} />
           <Route path="/eventos/:eventSlug/:editionYear" element={<PublicEditionPage />} />
+          <Route path="/autores/:authorName" element={<AuthorPage />} /> {/* Nova Rota */}
 
           {/* Rotas de Admin */}
           <Route path="/admin" element={<Navigate to="/admin/eventos" />} />
--- client/src/components/DocumentCard.js
+++ client/src/components/DocumentCard.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importar o Link
+import { Link } from 'react-router-dom';
 import './style/DocumentCard.css';
 
 function DocumentCard({ doc }) {
+  // Transforma a string de autores em links individuais
+  const authorLinks = doc.authors.split(',').map((author, index, array) => (
+    <span key={author.trim()}>
+      <Link to={`/autores/${encodeURIComponent(author.trim())}`}>
+        {author.trim()}
+      </Link>
+      {index < array.length - 1 ? ', ' : ''} {/* Adiciona vírgula entre os nomes */}
+    </span>
+  ));
+
   return (
     <div className="doc-card">
       <h3 className="doc-title">{doc.title}</h3>
-      <p className="doc-authors">{doc.authors}</p>
+      <p className="doc-authors">{authorLinks}</p>
       
-      {/* O nome da publicação agora é um link */}
       <p className="doc-publication">
         Publicado em: 
         <Link to={`/eventos/${doc.eventSlug}`}> {doc.publication}, {doc.year}</Link>
